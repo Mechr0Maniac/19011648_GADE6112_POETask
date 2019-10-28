2b1881bf04195f0958e48fb6558856b68089ddc2
Mechr0Maniac <hexorcast@gmail.com>
2019-10-28 08:57:19 +0200

    Removed test, added control script to camera

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public GameObject Sprites;
+    public int size;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Display();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Display()
+    {
+        size *= 3;
+        for (int x = 0; x < size; x += 3)
+        {
+            for (int z = 0; z < size; z += 3)
+            {
+                Instantiate(Sprites, new Vector2(x, z), Quaternion.identity);
+            }
+        }
+
+    }
+}
